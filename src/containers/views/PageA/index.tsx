f29bb63f34de35b88dcc664fa3a759821e6a80d8
oxc <524112857@qq.com>
2022-10-13 14:50:35 +0800

    init: 支持打包分包以及插入环境变量

--- src/containers/views/PageA/index.tsx
+++ src/containers/views/PageA/index.tsx
@@ -4,15 +4,18 @@ import { useNavigate } from 'react-router-dom'
 import { observer } from 'mobx-react'
 import { getStores } from '@store/index'
 
+import styles from './index.scss'
+
 const PageA = () => {
     const navigate = useNavigate()
     const { numStore } = getStores()
     return (
-        <div>
+        <div className={styles.ttest}>
             pagea
             {numStore.num}
             <Button onClick={() => navigate('/page-b')}>点击</Button>
             <Button onClick={() => numStore.increase()}>+</Button>
+            <div>{process.env.NODE_ENV}</div>
         </div>
     )
 }
